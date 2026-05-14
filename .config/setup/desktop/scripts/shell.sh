2d55bc606ceacd30459360dec27d8f47083f137f
Encephala <jonathan_rietveld@hotmail.com>
2026-05-14 17:04:04 +0200

    Fix .zshrc

--- .config/setup/desktop/scripts/shell.sh
+++ .config/setup/desktop/scripts/shell.sh
@@ -4,10 +4,8 @@ set -euo pipefail
 # ── Oh My Zsh ─────────────────────────────────────────────────────────────────
 
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 fi
-
-# ── Default shell ─────────────────────────────────────────────────────────────
 
 if [[ "$SHELL" != "/usr/bin/zsh" ]]; then
     chsh -s /usr/bin/zsh
--- .zshrc
+++ .zshrc
@@ -1,15 +1,10 @@
 export ZSH="$HOME/.oh-my-zsh"
 
-CASE_SENSITIVE="true"
-HIST_STAMPS="yyyy-mm-dd"
+ZSH_THEME=""
 
-plugins=(per-directory-history zoxide emoji-clock diff-so-fancy)
-
-source $HOME/.cargo/env
+plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
 
-export PATH="$HOME/.local/bin:$PATH"
-export EDITOR="vim"
-
 eval "$(starship init zsh)"
+eval "$(zoxide init zsh)"
