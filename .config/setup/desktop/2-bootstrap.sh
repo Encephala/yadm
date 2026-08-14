5499a6f4e7374746b8603257501af19c8f17e3b9
Encephala <jonathan_rietveld@hotmail.com>
2026-08-14 19:03:33 +0200

    Start with pacman all packages update

--- .config/setup/desktop/2-bootstrap.sh
+++ .config/setup/desktop/2-bootstrap.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+echo "==> system update"
+sudo pacman -Syu --noconfirm
+
 echo "==> packages"
 bash "$SCRIPT_DIR/scripts/packages.sh"
 
